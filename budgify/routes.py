52bfedb774f1b823489b9508f86a0db65c586e6a
Ant <ant.griffiths22@gmail.com>
2023-08-29 23:30:38 +0100

    Set up register page route and template

--- budgify/routes.py
+++ budgify/routes.py
@@ -11,3 +11,8 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
