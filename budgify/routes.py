67a1647dfa862eb12d269f83ee051617deee8498
Ant <ant.griffiths22@gmail.com>
2023-08-29 23:20:47 +0100

    Set up login page route and template

--- budgify/routes.py
+++ budgify/routes.py
@@ -6,3 +6,8 @@ from budgify.models import User, BudgetPlanner, Transaction
 @app.route("/")
 def home():
     return render_template("welcome.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
