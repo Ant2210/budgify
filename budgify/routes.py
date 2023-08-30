a52c60ee4cb429e7a28b4924a7a494f66a34b010
Ant <ant.griffiths22@gmail.com>
2023-08-30 01:17:11 +0100

    Create register function

--- budgify/routes.py
+++ budgify/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import flash, redirect, render_template, request, session, url_for
 from budgify import app, db
 from budgify.models import User, BudgetPlanner, Transaction
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 @app.route("/")
@@ -15,4 +16,26 @@ def login():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+        if request.method == "POST":
+            # Check if username already exists in DB
+            existing_user = User.query.filter_by(
+                username=request.form.get("username")
+            ).first()
+
+            if existing_user:
+                flash("Username already exists, please choose another.")
+                return redirect(url_for("register"))
+
+            # Create new user
+            new_user = User(
+                username=request.form.get("username").lower(),
+                password=generate_password_hash(request.form.get("password")),
+            )
+            db.session.add(new_user)
+            db.session.commit()
+
+            # Log in new user
+            session["user"] = request.form.get("username")
+            flash("Registration Successful!")
+
+        return render_template("register.html")
