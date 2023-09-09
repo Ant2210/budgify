07a6e854094f325387e18b3a377735eafce440f5
Ant <ant.griffiths22@gmail.com>
2023-09-09 23:03:50 +0100

    FEAT: Add robots.txt file01~

--- budgify/__init__.py
+++ budgify/__init__.py
@@ -1,3 +1,4 @@
+from budgify import routes
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -6,10 +7,9 @@ if os.path.exists("env.py"):
 
 
 # Create an instance of Flask and store it in a variable called app
-app = Flask(__name__)
+# Set the path to the static folder in order to serve the robots.txt file, code found here https://shorturl.at/nwFR5
+app = Flask(__name__, static_folder='static', static_url_path='')
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
-
-from budgify import routes
