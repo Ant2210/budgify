c38c95aafa524dc6ad24bba366ac0b87442ec47d
Ant <ant.griffiths22@gmail.com>
2023-09-09 23:20:23 +0100

    Add requirements for deployment with live db

--- budgify/__init__.py
+++ budgify/__init__.py
@@ -10,6 +10,13 @@ if os.path.exists("env.py"):
 # Set the path to the static folder in order to serve the robots.txt file, code found here https://shorturl.at/nwFR5
 app = Flask(__name__, static_folder='static', static_url_path='')
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 db = SQLAlchemy(app)
