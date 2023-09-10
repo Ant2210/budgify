3c10f1d3f959f17ee504b7e012f3ce3d3396c00b
Ant <ant.griffiths22@gmail.com>
2023-09-10 23:29:30 +0100

    REFAC: Refactored confirmpwd.js for readability

--- budgify/static/js/confirmpwd.js
+++ budgify/static/js/confirmpwd.js
@@ -1,13 +1,20 @@
-let password = document.getElementById("password")
+/*
+ * Load this script on the register page and profile page where new
+ * passwords are being created to confirm that the password and confirm
+ * password fields match and provide a custom html validation message
+ * if they do not match.
+ */
+
+let password = document.getElementById("password");
 let confirm_password = document.getElementById("confirm-password");
 
 const confirmPassword = () => {
-  if(password.value != confirm_password.value) {
+  if (password.value != confirm_password.value) {
     confirm_password.setCustomValidity("Passwords do not match");
   } else {
-    confirm_password.setCustomValidity('');
+    confirm_password.setCustomValidity("");
   }
-}
+};
 
 password.onchange = confirmPassword;
 confirm_password.onkeyup = confirmPassword;
