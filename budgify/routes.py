7fb36f01569fdc30d9d6c75030e8977d0768aff2
Ant <ant.griffiths22@gmail.com>
2023-09-11 00:45:33 +0100

    REFAC: Refactor routes.py for readability

--- budgify/routes.py
+++ budgify/routes.py
@@ -4,42 +4,46 @@ from budgify.models import TransactionType, User, BudgetPlanner, Transaction
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
+# If no user redirect to the welcome page, otherwise redirect to the users
+# budgets page
 @app.route("/")
 def home():
     if "user" in session:
         return redirect(url_for("budgets", username=session["user"]))
-    
+
     return render_template("welcome.html", hide_navbar=True)
 
 
+# Register a new user
 @app.route("/register", methods=["GET", "POST"])
 def register():
-        if request.method == "POST":
-            # Check if username already exists in DB
-            existing_user = User.query.filter_by(
-                username=request.form.get("username")
-            ).first()
+    if request.method == "POST":
+        # Check if username already exists in DB
+        existing_user = User.query.filter_by(
+            username=request.form.get("username")
+        ).first()
 
-            if existing_user:
-                flash("Username already exists, please choose another.")
-                return redirect(url_for("register"))
+        if existing_user:
+            flash("Username already exists, please choose another.")
+            return redirect(url_for("register"))
 
-            # Create new user
-            new_user = User(
-                username=request.form.get("username").lower(),
-                password=generate_password_hash(request.form.get("password")),
-            )
-            db.session.add(new_user)
-            db.session.commit()
+        # Create new user
+        new_user = User(
+            username=request.form.get("username").lower(),
+            password=generate_password_hash(request.form.get("password")),
+        )
+        db.session.add(new_user)
+        db.session.commit()
 
-            # Log in new user
-            session["user"] = request.form.get("username").lower()
-            flash("Registration successful, let's create your first budget!")
-            return redirect(url_for("budgets", username=session["user"]))
+        # Log in new user
+        session["user"] = request.form.get("username").lower()
+        flash("Registration successful, let's create your first budget!")
+        return redirect(url_for("budgets", username=session["user"]))
 
-        return render_template("register.html")
+    return render_template("register.html")
 
 
+# Log in an existing user
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -54,21 +58,23 @@ def login():
                 existing_user.password, request.form.get("password")
             ):
                 session["user"] = request.form.get("username").lower()
-                flash("Welcome, {}!".format(request.form.get("username")).title())
+                flash("Welcome, {}!".format(
+                    request.form.get("username")).title())
                 return redirect(url_for("budgets", username=session["user"]))
             else:
                 # Invalid password match
                 flash("Incorrect username and/or password")
                 return redirect(url_for("login"))
-            
+
         else:
             # Username doesn't exist
             flash("Incorrect username and/or password")
             return redirect(url_for("login"))
-            
+
     return render_template("login.html")
 
 
+# Log out an already logged in user
 @app.route("/logout")
 def logout():
     # Remove the user from the session
@@ -77,26 +83,29 @@ def logout():
     return redirect(url_for("login"))
 
 
+# Display all the users budgets
 @app.route("/budgets/<username>")
 def budgets(username):
     # Check if the user is logged in
     if "user" not in session:
         flash("Please log in to view this page.")
         return redirect(url_for("login"))
-    
+
     # Check if the user has permission to access this page
     if session["user"] != username:
         flash("You do not have permission to access this page.")
         return redirect(url_for("budgets", username=session["user"]))
 
     # Retrieve the username from the User object
-    user_id = User.query.filter_by(username=session["user"]).with_entities(User.id).first_or_404()[0]
+    user_id = User.query.filter_by(
+        username=session["user"]).with_entities(User.id).first_or_404()[0]
     budgets = BudgetPlanner.query.filter_by(user_id=user_id).all()
-    
+
     # Pass the username as a variable to the "budgets" template
     return render_template("budgets.html", username=username, budgets=budgets)
 
 
+# Display a specific budget chosen by the user
 @app.route("/budget/<username>/<int:budget_id>", methods=["GET", "POST"])
 def budget(username, budget_id):
     # Check if the user is logged in
@@ -108,33 +117,50 @@ def budget(username, budget_id):
     if session["user"] != username:
         flash("You do not have permission to access this page.")
         return redirect(url_for("budgets", username=session["user"]))
-    
-    # Retrieve the username from the User object and budget information from the BudgetPlanner object
+
+    # Retrieve the username from the User object and budget information from
+    # the BudgetPlanner object
     budget = BudgetPlanner.query.get_or_404(budget_id)
     transaction_types = TransactionType.query.all()
-    transactions = Transaction.query.filter_by(budget_planner_id=budget_id).all()
+    transactions = Transaction.query.filter_by(
+        budget_planner_id=budget_id).all()
 
-        # Check if the budget belongs to the user
+    # Check if the budget belongs to the user
     if budget.user.username != username:
         flash("You do not have permission to access this page.")
         return redirect(url_for("budgets", username=session["user"]))
-    
+
     # Pass the username as a variable to the "add_budget" template
-    return render_template("budget.html", username=username, budget_id=budget_id, budget=budget, transaction_types=transaction_types, transactions=transactions)
+    return render_template(
+        "budget.html",
+        username=username,
+        budget_id=budget_id,
+        budget=budget,
+        transaction_types=transaction_types,
+        transactions=transactions
+    )
 
 
+# Add a new budget
 @app.route("/add_budget", methods=["POST"])
 def add_budget():
     budget_planner = BudgetPlanner(
-            name=request.form.get("budget_name"),
-            user_id=User.query.filter_by(username=session["user"]).with_entities(User.id).first_or_404()[0]
-        )
+        name=request.form.get("budget_name"),
+        user_id=User.query.filter_by(
+            username=session["user"]
+        ).with_entities(User.id).first_or_404()[0]
+    )
     db.session.add(budget_planner)
     db.session.commit()
     flash("Budget created successfully, lets add some transactions!")
-    return redirect(url_for("budget", username=session["user"], budget_id=budget_planner.id))
+    return redirect(
+        url_for(
+            "budget", username=session["user"], budget_id=budget_planner.id
+        )
+    )
 
 
+# Rename an existing budget
 @app.route("/rename_budget/<username>/<int:budget_id>", methods=["POST"])
 def rename_budget(username, budget_id):
     budget = BudgetPlanner.query.get_or_404(budget_id)
@@ -147,9 +173,12 @@ def rename_budget(username, budget_id):
     budget.name = request.form.get("budget_name")
     db.session.commit()
     flash("Budget renamed successfully.")
-    return redirect(url_for("budget", username=session["user"], budget_id=budget.id))
+    return redirect(
+        url_for("budget", username=session["user"], budget_id=budget.id)
+    )
 
 
+# Delete an existing budget
 @app.route("/delete_budget/<username>/<int:budget_id>", methods=["POST"])
 def delete_budget(username, budget_id):
     budget = BudgetPlanner.query.get_or_404(budget_id)
@@ -165,19 +194,19 @@ def delete_budget(username, budget_id):
     return redirect(url_for("budgets", username=session["user"]))
 
 
+# Add a new transaction to an existing budget
 @app.route("/add_transaction/<username>/<int:budget_id>", methods=["POST"])
 def add_transaction(username, budget_id):
     # Retrieve the budget associated with the budget_id
     budget = BudgetPlanner.query.get_or_404(budget_id)
 
-
     # Check if the budget belongs to the user
     if budget.user.username != username:
         flash("You do not have permission to access this page.")
         return redirect(url_for("budgets", username=session["user"]))
-    
+
     # Create new transaction
-    transaction=Transaction(
+    transaction = Transaction(
         description=request.form.get("transaction_description"),
         amount=request.form.get("transaction_amount"),
         day_of_month=int(request.form.get("transaction_day")),
@@ -187,10 +216,16 @@ def add_transaction(username, budget_id):
     db.session.add(transaction)
     db.session.commit()
     flash("Transaction added successfully.")
-    return redirect(url_for("budget", username=session["user"], budget_id=budget_id))
+    return redirect(
+        url_for("budget", username=session["user"], budget_id=budget_id)
+    )
 
 
-@app.route("/edit_transaction/<username>/<int:budget_id>/<int:transaction_id>", methods=["POST"])
+# Edit an existing transaction
+@app.route(
+    "/edit_transaction/<username>/<int:budget_id>/<int:transaction_id>",
+    methods=["POST"]
+)
 def edit_transaction(username, budget_id, transaction_id):
     # Retrieve the budget associated with the budget_id
     budget = BudgetPlanner.query.get_or_404(budget_id)
@@ -210,10 +245,16 @@ def edit_transaction(username, budget_id, transaction_id):
     transaction.type_id = int(request.form.get("transaction_type"))
     db.session.commit()
     flash("Transaction updated successfully.")
-    return redirect(url_for("budget", username=session["user"], budget_id=budget_id))
+    return redirect(
+        url_for("budget", username=session["user"], budget_id=budget_id)
+    )
 
 
-@app.route("/delete_transaction/<username>/<int:budget_id>/<int:transaction_id>", methods=["POST"])
+# Delete an existing transaction
+@app.route(
+    "/delete_transaction/<username>/<int:budget_id>/<int:transaction_id>",
+    methods=["POST"]
+)
 def delete_transaction(username, budget_id, transaction_id):
     # Retrieve the transaction associated with the transaction_id
     transaction = Transaction.query.get_or_404(transaction_id)
@@ -222,13 +263,16 @@ def delete_transaction(username, budget_id, transaction_id):
     if transaction.budget_planner.user.username != username:
         flash("You do not have permission to access this page.")
         return redirect(url_for("budgets", username=session["user"]))
-    
+
     db.session.delete(transaction)
     db.session.commit()
     flash("Transaction deleted successfully.")
-    return redirect(url_for("budget", username=session["user"], budget_id=budget_id))
+    return redirect(
+        url_for("budget", username=session["user"], budget_id=budget_id)
+    )
 
 
+# Display the users profile page
 @app.route("/profile/<username>")
 def profile(username):
     # Check if the user is logged in
@@ -240,11 +284,12 @@ def profile(username):
     if session["user"] != username:
         flash("You do not have permission to access this page.")
         return redirect(url_for("budgets", username=session["user"]))
-    
+
     # Pass the username as a variable to the "profile" template
     return render_template("profile.html", username=username)
 
 
+# Change the users current password
 @app.route("/change_password/<username>", methods=["POST"])
 def change_password(username):
     # Check if the user is logged in
@@ -261,7 +306,9 @@ def change_password(username):
     user = User.query.filter_by(username=username).first_or_404()
 
     # Check if the old password matches
-    if not check_password_hash(user.password, request.form.get("current-password")):
+    if not check_password_hash(
+        user.password, request.form.get("current-password")
+    ):
         flash("Incorrect password, your password has not been updated.")
         return redirect(url_for("profile", username=username))
 
@@ -272,26 +319,29 @@ def change_password(username):
     return redirect(url_for("profile", username=username))
 
 
+# Delete the users account
 @app.route("/delete_account/<username>", methods=["POST"])
 def delete_account(username):
     # Check if the user is logged in
     if "user" not in session:
         flash("Please log in to view this page.")
         return redirect(url_for("login"))
-    
+
     # Check if the user has permission to access this page
     if session["user"] != username:
         flash("You do not have permission to access this page.")
         return redirect(url_for("profile", username=session["user"]))
-    
+
     # Retrieve the user associated with the username
     user = User.query.filter_by(username=username).first_or_404()
 
     # Check if the old password matches
-    if not check_password_hash(user.password, request.form.get("delete-account-password")):
+    if not check_password_hash(
+        user.password, request.form.get("delete-account-password")
+    ):
         flash("Incorrect password, your account has not been deleted.")
         return redirect(url_for("profile", username=username))
-    
+
     # Delete the user
     db.session.delete(user)
     db.session.commit()
